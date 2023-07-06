6c04b8e54098dc87c48483f8da9fcbd562cbfa8a
marcobusacca <marcobusacca01@gmail.com>
2023-07-06 17:12:01 +0200

    [FEAT] bootstrap cdn css

--- index.php
+++ index.php
@@ -7,12 +7,18 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Bootstrap CDN CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
         <!-- Collegamento a "style.css" -->
         <link rel="stylesheet" href="./css/style.css">
+
         <!-- Vue.js CDN -->
         <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
         <!-- Collegamento a "script.js" -->
         <script defer type="text/javascript" src="./js/script.js"></script>
+        
         <!-- Head Title -->
         <title>php-todo-list-json</title>
     </head>
