fb0c2439084aad8b3862f42d3d700e0e4a3888b0
marcobusacca <marcobusacca01@gmail.com>
2023-07-06 17:12:33 +0200

    [FEAT] axios cdn

--- index.php
+++ index.php
@@ -16,9 +16,12 @@
         <!-- Vue.js CDN -->
         <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 
+        <!-- Axios CDN -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.4.0/axios.min.js" integrity="sha512-uMtXmF28A2Ab/JJO2t/vYhlaa/3ahUOgj1Zf27M5rOo8/+fcTUVH0/E0ll68njmjrLqOBjXM3V9NiPFL5ywWPQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
         <!-- Collegamento a "script.js" -->
         <script defer type="text/javascript" src="./js/script.js"></script>
-        
+
         <!-- Head Title -->
         <title>php-todo-list-json</title>
     </head>
