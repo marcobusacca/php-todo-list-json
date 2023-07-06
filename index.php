29b88d140a976f72a3b44abddaee55a34cb90a54
marcobusacca <marcobusacca01@gmail.com>
2023-07-06 17:09:53 +0200

    [FEAT] inizializzazione script.js e vue.js

--- index.php
+++ index.php
@@ -9,6 +9,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Collegamento a "style.css" -->
         <link rel="stylesheet" href="./css/style.css">
+        <!-- Vue.js CDN -->
+        <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+        <!-- Collegamento a "script.js" -->
+        <script defer type="text/javascript" src="./js/script.js"></script>
         <!-- Head Title -->
         <title>php-todo-list-json</title>
     </head>
---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// RICHIAMO FUNZIONE CREATE_APP VUE.JS E LA INSERISCO IN UNA CONSTANTE
+const { createApp } = Vue;
+
+// UTILIZZO FUNZIONE CREATE_APP
+createApp({
+    // DEFINIZIONE DATA CON L'OGGETTO RETURN
+    data() {
+        return {
+            // VARIABILI VUE.JS
+        }
+    }
+}).mount('#app'); // RICHIAMO IL CONTAINER HTML DOVE APPLICARE LA FUNZIONE CREATE_APP
