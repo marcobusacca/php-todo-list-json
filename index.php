45d260da64f6f7dcc0f265479780c8ec49d8378c
marcobusacca <marcobusacca01@gmail.com>
2023-07-06 17:06:48 +0200

    [FEAT] creazione e collegamento style.css

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Collegamento a "style.css" -->
+        <link rel="stylesheet" href="./css/style.css">
         <!-- Head Title -->
         <title>php-todo-list-json</title>
     </head>
